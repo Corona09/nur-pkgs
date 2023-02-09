e63e5f122a6d94d40aefdac58271c839e98063b4
Corona <Corona09@163.com>
2023-02-09 16:47:14 +0800

    添加了 dwm 等自定义包

--- default.nix
+++ default.nix
@@ -9,11 +9,22 @@
 { pkgs ? import <nixpkgs> { } }:
 
 {
-  # The `lib`, `modules`, and `overlay` names are special
-  lib = import ./lib { inherit pkgs; }; # functions
-  modules = import ./modules; # NixOS modules
-  overlays = import ./overlays; # nixpkgs overlays
+    # The `lib`, `modules`, and `overlay` names are special
+    lib = import ./lib { inherit pkgs; }; # functions
+    modules = import ./modules; # NixOS modules
+    overlays = import ./overlays; # nixpkgs overlays
 
-  cfft = pkgs.callPackage ./pkgs/cfft { };
-
+    cfft = pkgs.callPackage ./pkgs/cfft { };
+    dmenu = pkgs.callPackage ./pkgs/dmenu { };
+    dwm = pkgs.callPackage ./pkgs/dwm { };
+    dwmblocks = pkgs.callPackage ./pkgs/dwmblocks { };
+    fcitx5-pinyin-moegirl = pkgs.callPackage ./pkgs/fcitx5-pinyin-moegirl { };
+    fcitx5-pinyin-personal = pkgs.callPackage ./pkgs/fcitx5-pinyin-personal { };
+    fcitx5-pinyin-zhwiki = pkgs.callPackage ./pkgs/fcitx5-pinyin-zhwiki { };
+    gbkunzip = pkgs.callPackage ./pkgs/gbkunzip { };
+    pigchacli = pkgs.callPackage ./pkgs/pigchacli { };
+    qq = pkgs.callPackage ./pkgs/qq { };
+    st = pkgs.callPackage ./pkgs/st { };
+    templates = pkgs.callPackage ./pkgs/templates { };
+    todo = pkgs.callPackage ./pkgs/todo { };
 }
