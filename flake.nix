cd889e26edc1449615cb2f4e0c5d4185811021d6
Corona <Corona09@163.com>
2023-02-09 17:22:45 +0800

    set allowUnfree

--- flake.nix
+++ flake.nix
@@ -1,21 +1,29 @@
 {
-  description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+    description = "My personal NUR repository";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        flake-utils-plus.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    };
+    outputs = { self, nixpkgs, flake-utils-plus, ... }:
     let
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+        utils = flake-utils-plus.lib;
+        inherit (nixpkgs) lib;
+        systems = [
+            "x86_64-linux"
+            "i686-linux"
+            "x86_64-darwin"
+            "aarch64-linux"
+            "armv6l-linux"
+            "armv7l-linux"
+        ];
+        forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
-      packages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
+        packages = forAllSystems (system: import ./default.nix {
+            pkgs = import nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+            };
+        });
     };
 }
