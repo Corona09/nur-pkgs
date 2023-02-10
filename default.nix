9be6184f598463aeb11d97428e71f9625cfb8921
Corona <Corona09@163.com>
2023-02-10 15:51:17 +0800

    add picgo

--- default.nix
+++ default.nix
@@ -27,4 +27,5 @@
     st = pkgs.callPackage ./pkgs/st { };
     templates = pkgs.callPackage ./pkgs/templates { };
     todo = pkgs.callPackage ./pkgs/todo { };
+    picgo = pkgs.callPackage ./pkgs/picgo { };
 }
