05834938a882dae81d9bf48d4ea13f9e08d5a6e7
Corona <Corona09@163.com>
2023-03-10 20:48:37 +0800

    update adoc-utils

--- pkgs/adoc-utils/src/adoc2pdf.sh
+++ pkgs/adoc-utils/src/adoc2pdf.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-asciidoctor-pdf -a scripts=cjk -a pdf-theme=<out>/theme/theme.yml -a pdf-fontsdir=<out>/fonts -r asciidoctor-mathematical $@
+asciidoctor-pdf -a allow-uri-read \
+				-a scripts=cjk \
+				-a pdf-theme=<out>/theme/theme.yml \
+				-a pdf-fontsdir=<out>/fonts \
+				-r asciidoctor-mathematical $@
